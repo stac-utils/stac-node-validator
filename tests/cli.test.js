f4bd808a90551ab9ff637619204e8b03056a5e13
Victor Engmark <vengmark@linz.govt.nz>
2021-12-06 11:36:09 +0000

    tests: Group assertions (#34)
    * refactor: Remove unnecessary no-op function
    
    * refactor: Use const for constant value
    
    * tests: Group assertions
    
    This way, if one "subtest" fails the other ones can give more useful
    information about the failure.
    
    * refactor: Inline multiple empty initializations

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,39 +1,56 @@
 const app = require('../index');
 
-let mockExit;
-let consoleInfSpy;
-let consoleLogSpy;
-let consoleErrSpy;
-let initString = 'STAC Node Validator v1.1.0';
+let consoleErrSpy, consoleInfSpy, consoleLogSpy, mockExit;
+const initString = 'STAC Node Validator v1.1.0';
 
 beforeEach(() => {
-	mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-	consoleInfSpy = jest.spyOn(console, 'info').mockImplementation(() => {});
-	consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-	consoleErrSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+	mockExit = jest.spyOn(process, 'exit').mockImplementation();
+	consoleInfSpy = jest.spyOn(console, 'info').mockImplementation();
+	consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+	consoleErrSpy = jest.spyOn(console, 'error').mockImplementation();
 });
 
-it('Should return exit code 1 and an error when run without parameters', async () => {
-	await app({});
+it('Should print init string', async () => {
+	await app();
 
 	expect(consoleLogSpy.mock.calls[0][0]).toContain(initString);
-	expect(consoleErrSpy.mock.calls[0][0] instanceof Error).toBeTruthy();
-	expect(consoleErrSpy.mock.calls[0][0].message).toContain('No path or URL specified.');
-	expect(mockExit).toHaveBeenCalledWith(1);
 });
 
-it('Should return exit code 0 and no error when run against simple catalog', async () => {
-	await app({
-		files: ['tests/catalog.json']
+describe('Running without parameters or configuration', () => {
+	it('Should return exit code 1', async () => {
+		await app();
+		expect(mockExit).toHaveBeenCalledWith(1);
 	});
 
-	expect(consoleLogSpy.mock.calls[0][0]).toContain(initString);
-	expect(consoleLogSpy.mock.calls[1][0]).toContain('tests/catalog.json');
-	expect(consoleInfSpy.mock.calls[0][0]).toContain('Files: 1');
-	expect(consoleInfSpy.mock.calls[1][0]).toContain('Valid: 1');
-	expect(consoleInfSpy.mock.calls[2][0]).toContain('Invalid: 0');
-	expect(consoleErrSpy).not.toHaveBeenCalled();
-	expect(mockExit).toHaveBeenCalledWith(0);
+	it('Should print an error message', async () => {
+		await app();
+		expect(consoleErrSpy.mock.calls[0][0] instanceof Error).toBeTruthy();
+		expect(consoleErrSpy.mock.calls[0][0].message).toContain('No path or URL specified.');
+	});
+});
+
+describe('Running with a configured simple catalog', () => {
+	it('Should return exit code 0', async () => {
+		await app({files: ['tests/catalog.json']});
+
+		expect(mockExit).toHaveBeenCalledWith(0);
+	});
+
+	it('Should print informational messages', async () => {
+		await app({files: ['tests/catalog.json']});
+
+		expect(consoleLogSpy.mock.calls[0][0]).toContain(initString);
+		expect(consoleLogSpy.mock.calls[1][0]).toContain('tests/catalog.json');
+		expect(consoleInfSpy.mock.calls[0][0]).toContain('Files: 1');
+		expect(consoleInfSpy.mock.calls[1][0]).toContain('Valid: 1');
+		expect(consoleInfSpy.mock.calls[2][0]).toContain('Invalid: 0');
+	});
+
+	it('Should not print an error message', async () => {
+		await app({files: ['tests/catalog.json']});
+
+		expect(consoleErrSpy).not.toHaveBeenCalled();
+	});
 });
 
 afterEach(() => {
