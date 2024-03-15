776de0be46693071e9f1640e8f20b870cada52d3
Matthias Mohr <webmaster@mamo-net.de>
2024-03-15 10:56:42 +0100

    Implement temporary workaround for https://github.com/OSGeo/PROJ/issues/4088

--- src/loader/default.js
+++ src/loader/default.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 
 async function loader(uri) {
+	// Todo: Temporary workaround for https://github.com/OSGeo/PROJ/issues/4088
+	const projjson = uri.startsWith('https://proj.org/schemas/');
+	if (projjson) {
+		uri = uri.replace('https://proj.org/schemas/', 'https://proj.org/en/latest/schemas/');
+	}
+
 	let response = await axios.get(uri);
 	return response.data;
 }
