b2f7fe59e872509a775c30a87fa6b58983cfd9c4
Scott Ellis <Scott.Ellis@nv5.com>
2025-02-13 10:17:12 -0700

    Allow host to be empty string for local file:/// paths

--- iri.js
+++ iri.js
@@ -20,7 +20,7 @@ module.exports = {
 		}
 
 		const iri = parse(value);
-		if ((iri.reference === 'absolute' || iri.reference === 'uri') && iri.scheme && iri.host) {
+		if ((iri.reference === 'absolute' || iri.reference === 'uri') && iri.scheme && (iri.host !== undefined)) {
 			return true;
 		}
 
