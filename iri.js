1f260a77ef9378ae9da70e3e91ebb411ce9e4c01
Scott Ellis <Scott.Ellis@nv5.com>
2025-03-13 10:09:48 -0600

    update from PR suggestion to be file scheme specific

--- iri.js
+++ iri.js
@@ -20,7 +20,7 @@ module.exports = {
 		}
 
 		const iri = parse(value);
-		if ((iri.reference === 'absolute' || iri.reference === 'uri') && iri.scheme && typeof iri.host !== 'undefined') {
+		if ((iri.reference === 'absolute' || iri.reference === 'uri') && iri.scheme && (iri.host || (iri.scheme === 'file' && iri.path))) {
 			return true;
 		}
 
