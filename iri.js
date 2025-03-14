5c8404d57b33dc4af8df90758b9aac1c805c8b8b
Matthias Mohr <webmaster@mamo-net.de>
2025-03-14 17:07:43 +0100

    Support for file URIs

--- iri.js
+++ iri.js
@@ -8,7 +8,7 @@ module.exports = {
 		}
 
 		const iri = parse(value);
-		if ((iri.reference === 'absolute' || iri.reference === 'uri') && iri.scheme && iri.host) {
+		if ((iri.reference === 'absolute' || iri.reference === 'uri') && iri.scheme && (iri.host || (iri.scheme === 'file' && iri.path))) {
 			return true;
 		}
 
